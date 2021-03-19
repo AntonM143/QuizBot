c9393d1565dbf1e077bfbf132593eb14f78044d1
robinhlgrsn <robinholgersson91@gmail.com>
2021-03-19 12:22:07 +0100

    styling-changes

--- game.js
+++ game.js
@@ -1,21 +1,26 @@
 window.addEventListener("load", startGame);
 let guessButton = document.getElementById("guessNumberBtn");
-guessButton.addEventListener("click", checkAnswer);
+guessButton.addEventListener("click", () => {
+    let inputNumber = document.getElementById("inputNumber")
+    checkAnswer()
+    inputNumber.focus()
+});
+
 let maxNumber = 10;
 let randomNumber = Math.floor(Math.random() * maxNumber);
 
 function startGame(){
     console.log("%cTHE GAME WAS STARTED", "color: green; font-size: 20px;")
-    let box = document.getElementById("demo");
+    let box = document.getElementById("botInfo");
     box.innerText = "Start guessing!";
     let inputNumber = document.getElementById("inputNumber").value = " ";
-    
 }
 function checkAnswer(){
-    let box = document.getElementById("demo");
+    let box = document.getElementById("botInfo");
     let inputNumber = document.getElementById("inputNumber").value;
     console.log("The right answer is: " + randomNumber);
     console.log("Your answer is: " + inputNumber);
+
     if(isNaN(inputNumber)){
         box.innerText = "You must choose a number!";
         inputNumber = " ";
@@ -23,8 +28,10 @@ function checkAnswer(){
     }
     if(inputNumber > randomNumber){
         box.innerText = "Lower!";
+        let inputNumber = document.getElementById("inputNumber").value = " ";
     }if(inputNumber < randomNumber){
         box.innerText = "Higher!";
+        let inputNumber = document.getElementById("inputNumber").value = " ";
     }
     if(inputNumber == randomNumber){
         box.innerText = "You won!!";
