4cb5f39547a511a2005d4c9e87533621b5dd67c0
Sara Oscarsson <oscarsson.sara@hotmail.com>
2021-03-24 11:47:54 +0100

    checkIfLoggedIn-logout

--- JS/login.js
+++ JS/login.js
@@ -19,16 +19,4 @@ function login(){
     }
     infoBox.innerText = "Username or password is incorrect..";
 }
-/* dessa funktioner ska läggas i main.js och game.js för att kunna logga ut och även kunna kolla
-om någon är inloggad */
-function checkLogin(){
-    let loggedInUser = JSON.parse(localStorage.getItem("login"));
-    if(loggedInUser == null){
-        location.replace("login.html");
-    }
-}
-function logout(){
-    let loggedInUser = JSON.parse(localStorage.getItem("login"));
-    loggedInUser = null;
-    localStorage.setItem("login", JSON.stringify(loggedInUser));
-}
+
